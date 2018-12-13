d88103cecc263577d9d212139f7328b4516bf378
Srikanth Nandiraju <nandiraju@yahoo.com>
2018-12-13 08:48:25 -0800

    Kind Meter

--- js/jquery.thermometer.js
+++ js/jquery.thermometer.js
@@ -241,7 +241,7 @@
 			tickColour: '#000000',
 			liquidColour: "#ff0000",
 			animationSpeed: 1000,
-			pathToSVG: "/svg/thermo-bottom.svg",
+			pathToSVG: "svg/thermo-bottom.svg",
 			valueChanged: undefined,
 			onLoad: undefined
 		}
